5c53ec1fceb0ee2d89c183cf01cc5ed29bf38398
pedrogsmartini <146433221+pedrogsmartini@users.noreply.github.com>
2023-10-22 00:26:47 +0000

    Prime numbers tested at home

---
+++ homeworks/isPrime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int PrimePredictor(int num)
+{
+    int i = 1;
+    int cnt = 0;
+    while (i<=num)
+    {
+       if (num%i == 0)
+       {
+        cnt++;
+       }
+       i++;
+    }
+    if (cnt ==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+int main()
+{
+    int num;
+    printf("Type a number:\n");
+    scanf("%d",&num);
+    int sol = PrimePredictor(num);
+    printf("%d\n",sol);
+
+}
