3f1dd56d35289f2906e108e1b7bbe07ac74efb40
pedrogsmartini <146433221+pedrogsmartini@users.noreply.github.com>
2023-11-02 20:50:14 +0000

    First assessment done

--- homeworks/StepsTask1.c
+++ homeworks/StepsTask1.c
@@ -1,57 +1,82 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	char steps[6]; 
+} FITNESS_DATA;
+
+// Define any additional variables here
+char date[100]; 
+char time[100];   
+char steps[100];
+
+char filename[] = "FitnessData_2023.csv";
+int rec = 0;
+int i = 1;
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() 
 {
-
-    char filename[] = "FitnessData_2023.csv";
     FILE *file = fopen(filename,"r");
     if (filename == NULL)
     {
         perror("No such file!");
         return 1;
     }
-        //To read files, we need a buffer. What is a buffer?
-    //Its the amount that the code will read from your
-    //file that you desire.
     int buffer_size = 150;
     char line_buffer[buffer_size];
-    int rec = 0;
-    int i = 1;
-    while (fgets(line_buffer, buffer_size, file) !=NULL & i<=3)
+    FITNESS_DATA initdata[3];
+    while (fgets(line_buffer, buffer_size, file))
     {
-        char var = 'o';
-        int cnt=-1;
-        while (var!='\0')
-        {
-            cnt++;
-            var=line_buffer[cnt];
-            if (var==',')
-            {
-                line_buffer[cnt] = '/';
-            }
         
+        if (rec<=2)
+        {
+            tokeniseRecord(line_buffer, ",", date, time, steps);
+            strcpy(initdata[rec].date, date);
+            strcpy(initdata[rec].time, time);
+            strcpy(initdata[rec].steps, steps);
         }
-        printf("%s", line_buffer);       
-        //Right on! Right now, you are on the loop of the file reading
-        //line by line. Do the commands you want referring to the 
-        //current line of the loop.
-        i++;
-        //printf("%s\n",line_buffer);
-        //int a = atoi(line_buffer);
-    }
-
-    buffer_size = 150;
-    line_buffer[buffer_size];
-    rec = 0;
-    i = 1;
-
-    while (fgets(line_buffer, buffer_size, file) !=NULL)
-    {
         rec++;
     }
-    printf("Number of records: %d\n",rec);
+    printf("Number of records in file: %d\n",rec);
+    int steps0 = atoi(initdata[0].steps);
+    int steps1 = atoi(initdata[1].steps);
+    int steps2 = atoi(initdata[2].steps);
+    printf("%s/%s/%d\n",initdata[0].date,initdata[0].time,steps0);
+    printf("%s/%s/%d\n",initdata[1].date,initdata[1].time,steps1);
+    printf("%s/%s/%d\n",initdata[2].date,initdata[2].time,steps2);
     fclose(file);
-    
     return 0;
 }
